ae2b8b320200dc6881a7f86e88b37bc2eef57dc3
wilrona <wilrona@gmail.com>
2017-12-26 17:29:11 +0100

    initialisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {NativePageTransitions} from "@ionic-native/native-page-transitions";
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    NativePageTransitions
   ]
 })
 export class AppModule {}
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import {NavController} from "ionic-angular";
+import {NativePageTransitions, NativeTransitionOptions} from "@ionic-native/native-page-transitions";
 
 @Component({
   templateUrl: 'tabs.html'
@@ -13,7 +15,43 @@ export class TabsPage {
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
-  constructor() {
+  loaded:   boolean = false;
+  tabIndex: number  = 0;
 
+  constructor(public navCtrl: NavController, private  nativePageTransitions: NativePageTransitions) {
+
+  }
+
+  private getAnimationDirection(index):string {
+    var currentIndex = this.tabIndex;
+
+    this.tabIndex = index;
+
+    switch (true){
+      case (currentIndex < index):
+        return('left');
+      case (currentIndex > index):
+        return ('right');
+    }
+  }
+
+  public transition(e):void {
+    let options: NativeTransitionOptions = {
+      direction:this.getAnimationDirection(e.index),
+      duration: 250,
+      slowdownfactor: -1,
+      slidePixels: 0,
+      iosdelay: 20,
+      androiddelay: 0,
+      fixedPixelsTop: 0,
+      fixedPixelsBottom: 48
+    };
+
+    if (!this.loaded) {
+      this.loaded = true;
+      return;
+    }
+
+    this.nativePageTransitions.slide(options);
   }
 }
