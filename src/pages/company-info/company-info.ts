7934a50dc077ca3fdae2ddb7e2f77093462cf82a
wilrona <wilrona@gmail.com>
2018-01-05 18:00:12 +0100

    upload du logo

--- src/pages/company-info/company-info.ts
+++ src/pages/company-info/company-info.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import {IonicPage, NavController, NavParams, ViewController} from 'ionic-angular';
+import {IonicPage, NavController, NavParams, Platform, ViewController} from 'ionic-angular';
+
+// DONT FORGET THIS DECLARATION TO AVOID TYPESCRIPT ERROR
+declare var VideoPicturePreviewPickerV2: any;
 
 /**
  * Generated class for the CompanyInfoPage page.
@@ -15,7 +18,7 @@ import {IonicPage, NavController, NavParams, ViewController} from 'ionic-angular
 })
 export class CompanyInfoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, public platform: Platform) {
   }
 
   ionViewDidLoad() {
@@ -24,6 +27,27 @@ export class CompanyInfoPage {
 
   dismiss() {
     this.viewCtrl.dismiss();
+  }
+
+  uploadPicture()
+  {
+
+    this.platform.ready().then(() =>
+    {
+      VideoPicturePreviewPickerV2.openPicker(
+        function(results) {
+          console.log(results);
+        }, function (error) {
+          console.log(error);
+        }, {
+          limit_Select: 1,
+          Is_multiSelect: false,
+          picture_selector:  false,
+          video_selector:  false,
+          display_video_time: false,
+          display_preview: true
+        });
+    });
   }
 
 }
